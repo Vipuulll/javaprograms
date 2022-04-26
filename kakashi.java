471560363cc0526a8faf5acdc1dd26eb7326a7b9
Vipuulll <96220207+Vipuulll@users.noreply.github.com>
2022-04-26 23:12:18 +0530

    Create kakashi.java

---
+++ kakashi.java
@@ -0,0 +1,8 @@
+public class kakashi{
+  public static void main(String args[])
+  {
+    System.out.println("KAKASHI HATAKE");
+    System.out.println("Kakashi Hatake is introduced as the Jonin (elite ninja) leader in charge of Team 7, a new ninja team including Naruto Uzumaki, Sasuke Uchiha and Sakura Haruno");
+  }
+}
+                       
